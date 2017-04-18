56ce56fbc3af6e82684c5d24341206febb4d20f8
etu <etu@STRI>
2017-04-18 17:46:07 +0200

    Ajout de client.java et modif de profil.java

---
+++ Documents/src/model/Client.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Client extends Profil {
+	
+	public Client(String login, String mdp,	String nom, String prenom) {
+		super(login,mdp,nom,prenom);
+	}
+}
--- Documents/src/model/Profil.java
+++ Documents/src/model/Profil.java
@@ -1,5 +1,21 @@
 package model;
 
-public class Profil {
-
+public abstract class Profil {
+	private String login;
+	private String mdp;
+	private String nom;
+	private String prenom;
+	private boolean connecte=false;
+	
+	public Profil(String login, String mdp,	String nom, String prenom) {
+		this.login=login;
+		this.mdp=mdp;
+		this.nom=nom;
+		this.prenom=prenom;
+	}
+	
+	public void seConnecter() {
+		this.connecte=true;
+	}
+	
 }
