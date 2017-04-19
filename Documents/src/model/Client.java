ab5bc5b3fdf2e19f58b31a0049b027192bd2c39e
etu <etu@STRI>
2017-04-19 17:00:50 +0200

    Correctifs personnel.java

--- Documents/src/model/Client.java
+++ Documents/src/model/Client.java
@@ -2,8 +2,8 @@ package model;
 
 public class Client extends Profil {
 	
-	public Client(String login, String mdp,	String nom, String prenom) {
-		super(login,mdp,nom,prenom);
+	public Client(String mdp, String nom, String prenom) {
+		super(mdp,nom,prenom);
 	}
 	
 	public String toString() { 
