6c67d8ac6d78e5445e93e135058b01a6b03629ec
Boneshield <mathieutham@hotmail.fr>
2017-05-09 15:56:49 +0200

    Modif boundaryajouteralimentcarte

--- Documents/src/vue/BoundaryAjouterAlimentCarte.java
+++ Documents/src/vue/BoundaryAjouterAlimentCarte.java
@@ -1,5 +1,6 @@
 package vue;
 
+import model.ProfilUtilisateur;
 import control.ControlAjouterAlimentCarte;
 import control.ControlVerifierIdentification;
 import control.TypeAliment;
@@ -18,37 +19,39 @@ public class BoundaryAjouterAlimentCarte {
 	}
 	
 	public void ajouterAlimentCarte(int numProfil) {
-		
-		do
+		if(controlVerifierIdentification.verifierIdentification(ProfilUtilisateur.GERANT, numProfil))
 		{
-			System.out.println("1.Ajouter un hamburger");
-			System.out.println("2.Ajouter un accompagnement");
-			System.out.println("3.Ajouter une boisson");
-			this.choix=clavier.entreeClavierInt();
-			if(choix==1 || choix==2 || choix==3) {
-				EntreeValide = true;
-			}
-			else
+			do
 			{
-				System.out.println("Veuillez entrer 1, 2 ou 3");
+				System.out.println("1.Ajouter un hamburger");
+				System.out.println("2.Ajouter un accompagnement");
+				System.out.println("3.Ajouter une boisson");
+				this.choix=clavier.entreeClavierInt();
+				if(choix==1 || choix==2 || choix==3) {
+					EntreeValide = true;
+				}
+				else
+				{
+					System.out.println("Veuillez entrer 1, 2 ou 3");
+				}
+			}while(!EntreeValide);
+			
+			System.out.println("Veuillez entrer le nom de l'aliment");
+			String nom = clavier.entreeClavierString();
+			switch (choix)
+			{
+				case 1:
+					controlAjouterAlimentCarte.ajouterAliment(TypeAliment.HAMBURGER, nom);
+					break;
+				case 2:
+					controlAjouterAlimentCarte.ajouterAliment(TypeAliment.ACCOMPAGNEMENT, nom);
+					break;
+				case 3:
+					controlAjouterAlimentCarte.ajouterAliment(TypeAliment.BOISSON, nom);
+					break;
+				default:
+					System.out.println("Type d'aliment nom reconnu");
 			}
-		}while(!EntreeValide);
-		
-		System.out.println("Veuillez entrer le nom de l'aliment");
-		String nom = clavier.entreeClavierString();
-		switch (choix)
-		{
-			case 1:
-				controlAjouterAlimentCarte.ajouterAliment(TypeAliment.HAMBURGER, nom);
-				break;
-			case 2:
-				controlAjouterAlimentCarte.ajouterAliment(TypeAliment.ACCOMPAGNEMENT, nom);
-				break;
-			case 3:
-				controlAjouterAlimentCarte.ajouterAliment(TypeAliment.BOISSON, nom);
-				break;
-			default:
-				System.out.println("Type d'aliment nom reconnu");
 		}
 	}
 }
