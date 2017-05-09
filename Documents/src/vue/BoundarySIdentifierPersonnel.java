04c852790781aa65ea8daa22c0602e3a3b05001c
Boneshield <mathieutham@hotmail.fr>
2017-05-09 15:07:01 +0200

    Corrections VerifierId

--- Documents/src/vue/BoundarySIdentifierPersonnel.java
+++ Documents/src/vue/BoundarySIdentifierPersonnel.java
@@ -4,12 +4,16 @@ import control.ControlSIdentifier;
 
 public class BoundarySIdentifierPersonnel {
 
+	private ControlSIdentifier controlSIdentifier;
+	
+	private Clavier clavier = new Clavier();
+	
 	public BoundarySIdentifierPersonnel(ControlSIdentifier controlSIdentifier) {
-		// TODO Auto-generated constructor stub
+		this.controlSIdentifier = controlSIdentifier;
 	}
 
 	public void identificationPersonnel() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
