fe8436864b2700a2242d26877db4cab82e222d10
kaiarch <kakaiarch@gmail.com>
2017-05-24 15:36:52 +0200

    cas Archiver

--- Documents/src/control/ControlArchiver.java
+++ Documents/src/control/ControlArchiver.java
@@ -3,9 +3,14 @@ package control;
 import model.BDCommande;
 
 public class ControlArchiver {
-
+    private BDCommande bdCommande;
+    
 	public ControlArchiver(BDCommande bdCommande) {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void setObserver(BoundaryArchiver vue) {
+        BDCommande.getInstance().addObserver(vue);
 	}
 
 }
--- Documents/src/vue/BoundaryArchiver.java
+++ Documents/src/vue/BoundaryArchiver.java
@@ -2,19 +2,38 @@ package vue;
 
 import control.ControlArchiver;
 import control.ControlVerifierIdentification;
+import model.ProfilUtilisateur;
 
 public class BoundaryArchiver {
-
+    private Fichier fichier = new Fichier();
+    private ControlVerifierIdentification controlVerifierIdentification;
+	private ControlArchiver controlArchiver;
 	
 	public BoundaryArchiver(
 			ControlVerifierIdentification controlVerifierIdentification,
 			ControlArchiver controlArchiver) {
-		// TODO Auto-generated constructor stub
+		this.controlVerifierIdentification = controlVerifierIdentification;
+		this.controlArchiver = controlArchiver;
 	}
 
 	public void archiver(int numGerant) {
-		// TODO Auto-generated method stub
-		
+		if(controlVerifierIdentification.verifierIdentification(ProfilUtilisateur.GERANT, numGerant)){
+		    fichier.effacer();
+		}
 	}
+	
+	public void update(Observable arg0, Object arg1) {
+        if (arg0 instanceof BDCommande) {
+            if (arg1 instanceof Integer) {
+                Integer numLabel = (Integer) arg1;
+                if(numLabel == 2){
 
+                }
+            }else{
+                System.out.println("type de message inconnu");
+            }
+        }else{
+            System.out.println("Objet emetteur inconnue");
+        }
+    }
 }
